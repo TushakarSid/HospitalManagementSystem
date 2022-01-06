603d5c37fd2f7ea3bc56c38a5b26c6f0d191e5e3
Siddhant Gupta <siddhantgupta384@gmail.com>
2022-01-06 18:28:03 +0530

    SignIn Updated

--- backend/routes/Doctors.js
+++ backend/routes/Doctors.js
@@ -1,30 +1,87 @@
-const router = require('express').Router();
-let DocDetails = require('../models/DocDetails.model');
+const router = require('express').Router()
+let DocDetails = require('../models/DocDetails.model')
+const bcrypt = require('bcrypt')
 
 router.route('/').get((req, res) => {
   DocDetails.find()
-    .then(DocDetails => res.json(DocDetails))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+    .then((DocDetails) => res.json(DocDetails))
+    .catch((err) => res.status(400).json('Error: ' + err))
+})
+
 
 router.route('/add').post((req, res) => {
-  const docFName = req.body.docFName;
-  const docLName = req.body.docLName;
-  const mobile = req.body.mobile;
-  const email = req.body.email;
-  const password = req.body.password;
+  const docFName = req.body.docFName
+  const docLName = req.body.docLName
+  const mobile = req.body.mobile
+  const email = req.body.email
+  const password = req.body.password
 
   const newDoc = new DocDetails({
     docFName,
     docLName,
     mobile,
     email,
-    password
+    password,
+  })
+
+  newDoc
+    .save()
+    .then(() => res.json('Doctor Registered!'))
+    .catch((err) => res.status(400).json('Error: ' + err))
+})
+
+router.route('/comparePasswordByEmail').post(async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  console.log(email)
+  // var actpass
+  // try{
+    const actPass = await DocDetails.find({ email: email })
+    console.log(actPass.length)
+    if(actPass.length==0){
+      return res.json({success: false, message: 'Try Signing In'})
+
+    }
+  // }
+  // catch{
+  //   console.log("no such email in db")
+  // }
+  console.log(actPass[0].password)
+  console.log(password)
+
+  bcrypt.compare(password,  actPass[0].password, function(err, response) {
+    if (err){
+      // handle error
+      console.log(err)
+      return res.json({success: false, message: 'Techniacl Issue! Try again later'})
+    }
+    if (response){
+      // Send JWT
+      return res.json({success: true, message: 'Passwords do  match!'});
+    } 
+    else {
+      // response is OutgoingMessage object that server response http request
+      return res.json({success: false, message: 'Passwords do not match'});
+    }
   });
 
-  newDoc.save()
-    .then(() => res.json('Doctor Registered!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
 
-module.exports = router;
+  const result = await bcrypt.compare(password, actPass[0].password)
+  return result
+  console.log(result)
+  try {
+  } catch (error) {
+    console.log('Error while comparing password!', error.message)
+  }
+  // const newDoc = new DocDetails({
+  //   email,
+  //   password
+  // });
+
+  // newDoc.save()
+  //   .then(() => res.json('Doctor Registered!'))
+  //   .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router
--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,39 +1,163 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import { FaWindows } from 'react-icons/fa'
+import axios from 'axios'
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div className="outer">
-          <div className="inner">
+
+    constructor(props) {
+        super(props)
+
         
-            <form>
+        this.onChangeEmail = this.onChangeEmail.bind(this)
+        this.onChangePassword = this.onChangePassword.bind(this)
+        this.onChangeCategory = this.onChangeCategory.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
 
-                <h3>Log in</h3>
-
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-            </div>
-        </div>
-        );
+        this.state = {
+            email: '',
+            password: '',
+            unregistered: 'true',
+            category: ''
+        }
     }
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value,
+        })
+    }
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value,
+        })
+    }
+     onChangeCategory(e) {
+        // const { name, value } = e.target;
+        this.setState({
+            category: e.target.value,
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+        const doctor = {
+            email: this.state.email,
+            password: this.state.password,
+        }
+
+        if(this.state.category == "Doctor"){
+            console.log("here")
+
+            axios
+                .post('http://localhost:5000/doctor/comparePasswordByEmail/', doctor)
+                .then((res) => {
+
+                    if(res.data.message =="Try Signing In"){
+                        this.setState({
+                            unregistered:false
+                        })
+                        
+                    }
+                    else {
+                        window.location = "http://localhost:3000/"  
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                
+            // this.setState({
+            //     email: '',
+            //     password: '',
+            //     category: '',
+            // })
+            // console.log("heeeeee")
+        }
+        else{
+            console.log("for patient!")
+        }
+
+
+    }
+
+  render() {
+    return (
+      <div className="outer">
+        <div className="inner">
+          <form onSubmit={this.onSubmit}>
+            <h3>Log in</h3>
+   
+
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={this.onChangeEmail}
+                required
+
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={this.onChangePassword}
+                required
+
+
+              />
+            </div>
+            <div className="form-group radio">
+              <label style={{ margin: 10 }}>
+                <input
+                  type="radio"
+                  value="Patient"
+                  name="Category"
+                  onChange={this.onChangeCategory}
+                  required
+                />
+                <span>Patient</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Doctor"
+                  name="Category"
+                  onChange={this.onChangeCategory}
+                />
+                <span>Doctor</span>
+              </label>
+            </div>
+            
+            {(this.state.unregistered===false)?<div style={{color:'Red'}}> Register Yourself </div>:<div></div>}
+
+            {/* <div className="form-group">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div> */}
+
+            <button type="submit" className="btn btn-dark btn-lg btn-block">
+              Sign in
+            </button>
+            <p className="forgot-password text-right">
+              Forgot <a href="#">password?</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
